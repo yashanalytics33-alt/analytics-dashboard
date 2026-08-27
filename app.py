a8e789e19a0166308982d9879d6cdf3449977fe8
yashanalytics33-alt <yashanalytics33@gmail.com>
2026-08-27 17:51:55 +0530

    Update app.py

--- app.py
+++ app.py
@@ -181,37 +181,14 @@ with col2:
 with col3:
     created_range = st.date_input(
         "Created",
-        value=None,
+        value=(df["created"].min().date(), df["created"].max().date()),
         min_value=df["created"].min().date(),
         max_value=df["created"].max().date(),
-        format="DD MMM YYYY",
+        format="DD/MM/YYYY",
         key="created_filter"
     )
 
-if created_range is not None:
 
-    if isinstance(created_range, tuple):
-
-        if len(created_range) == 2:
-            start_date = created_range[0]
-            end_date = created_range[1]
-
-            filtered_df = filtered_df[
-                (filtered_df["created"].dt.date >= start_date) &
-                (filtered_df["created"].dt.date <= end_date)
-            ]
-
-        elif len(created_range) == 1:
-            start_date = created_range[0]
-
-            filtered_df = filtered_df[
-                filtered_df["created"].dt.date >= start_date
-            ]
-
-    else:
-        filtered_df = filtered_df[
-            filtered_df["created"].dt.date == created_range
-        ]
 
 # ---------------------------------------------------------
 # MONTH
@@ -461,6 +438,16 @@ with col14:
         placeholder="All"
     )
 
+
+# Created date range filter
+if isinstance(created_range, tuple) and len(created_range) == 2:
+    start_date = created_range[0]
+    end_date = created_range[1]
+
+    filtered_df = filtered_df[
+        (filtered_df["created"].dt.date >= start_date) &
+        (filtered_df["created"].dt.date <= end_date)
+    ]
 
 # =========================================================
 # APPLY FILTERS
